4a326a7df5b0808b293733e2934d5da3a0ff77fc
Innocent Suta <sutainnocent@gmail.com>
2021-06-14 12:20:02 +0300

    Authentication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,44 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
     public function index(){
         return view('auth.register');
+    }
+
+    public function store(Request $request){
+        
+        // validation
+        $this->validate($request, [
+            'name'=>'required | max:255',
+            'username'=>'required | max:255',
+            'email'=>'required | email | max:255',
+            'password'=>'required | confirmed',
+        ]);
+
+        // Store user
+        
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password'=> Hash::make($request->password),
+        ]);
+
+
+        // Sign the user in 
+
+        auth()->attempt($request->only('email','password'));
+
+        // Redirect the user
+
+        return redirect()->route('dashboard');
+
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,35 +4,61 @@
     <div class="flex justify-center">
         <div class="w-4/12 bg-white p-6 rounded-lg">
             <form action="{{route('register')}}" method="post">
+                @csrf
                 <div class="mb-4">
                     <label for="name" class="sr-only"> Name </label>
                     <input type="text" name="name" id="name" placeholder="Full Name" 
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" 
+                    value="{{old('name')}}">    
+
+                    @error('name')
+                        <div class="text-red-500 mt-2  text-sm">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="username" class="sr-only"> Username </label>
                     <input type="text" name="username" id="username" placeholder="Username" 
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg  @error('username') border-red-500 @enderror" value="{{old('username')}}">
+
+                     @error('username')
+                        <div class="text-red-500 mt-2  text-sm">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
             
                 <div class="mb-4">
                     <label for="email" class="sr-only"> Email </label>
                     <input type="text" name="email" id="email" placeholder="Your Email" 
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{old('email')}}">
+
+                     @error('email')
+                        <div class="text-red-500 mt-2  text-sm">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="password" class="sr-only"> Password </label>
                     <input type="password" name="password" id="password" placeholder="Password" 
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror" value="">
+
+                     @error('password')
+                        <div class="text-red-500 mt-2  text-sm">
+                            {{$message}}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
-                    <label for="confirm_password" class="sr-only"> Oonfirm Password </label>
-                    <input type="password" name="confirm_password" id="confirm_password" 
+                    <label for="password_confirmation" class="sr-only"> Confirm Password </label>
+                    <input type="password" name="password_confirmation" id="confirm_password" 
                      placeholder="Confirm Password" 
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror " value="">
                 </div>
                 <div>
                     <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
